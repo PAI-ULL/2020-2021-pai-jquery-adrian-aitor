7414228d4327ac97dc897bd56c1b473aca960c87
Adrian-alu0101024363 <alu0101024363@ull.edu.es>
2021-05-13 17:16:56 +0000

    fade

---
+++ src/js/fade.js
@@ -0,0 +1,4 @@
+$("#jquery").on('click', () => {
+  $('#alternative').fadeToggle();
+});
+
--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,16 +1,44 @@
+/**
+  @author  Adrian Fleitas
+  @author Aitor Hernández Sánchez
+  @since  28.April.2021
+  @desc Toggle class that show the vanilla and jquery way to toggle between html divs
+  @link https://github.com/PAI-ULL/2020-2021-pai-jquery-adrian-aitor.git
+*/
 
-  $("#test").on("click", function() {
-    $("#content").toggle();
-    $("#alternative").toggle();
-  });
 
-/*function toggleDiv() {
-  if(document.getElementById('content').style.display === 'block') {
-  document.getElementById('content').style.display = 'none';
-  document.getElementById('alternative').style.display = 'block';
-  } else {  
-  document.getElementById('alternative').style.display = 'none';
-  document.getElementById('content').style.display = 'block'
+'use strict';
+
+export class Toggle {
+
+  constructor() {
+    this.#listen();
   }
-} 
-document.getElementById('test').addEventListener('click', toggleDiv);*/
+
+  /** Jquery way to toggle between divs */
+  #jqueryToggleDiv() {
+    $("#jquery").on("click", function() {
+      $("#content").toggle();
+      $("#alternative").toggle();
+    });
+  }
+
+  /** Vanilla javascript way to toggle between divs */
+  #toggleDiv() {
+    if(document.getElementById('content').style.display === 'block') {
+    document.getElementById('content').style.display = 'none';
+    document.getElementById('alternative').style.display = 'block';
+    } else {  
+    document.getElementById('alternative').style.display = 'none';
+    document.getElementById('content').style.display = 'block'
+    }
+  } 
+
+  /** Function that initialize the listener and functions */
+  #listen() {
+    document.getElementById('vanilla').addEventListener('click', this.#toggleDiv);
+    this.#jqueryToggleDiv();
+  }
+}
+
+let toggle = new Toggle();
