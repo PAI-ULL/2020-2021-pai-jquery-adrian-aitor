c56dca71ff503a55f01795edb2823c176db30256
Adrian-alu0101024363 <alu0101024363@ull.edu.es>
2021-05-18 16:46:32 +0000

    shake class

---
+++ src/js/shake.js
@@ -0,0 +1,52 @@
+/**
+  @author  Adrian Fleitas
+  @author Aitor Hernández Sánchez
+  @since  28.April.2021
+  @desc Fade class that show the vanilla and jquery way to fade toggle an element
+  @link https://github.com/PAI-ULL/2020-2021-pai-jquery-adrian-aitor.git
+*/
+
+'use strict';
+
+export class Shake {
+
+  /** Calls the method that listen to events and do the actions */
+  constructor() {
+    this.#shake();
+    this.#over();
+  }
+  /** First shake the image then replace it with the after version
+   * If the after is clicked apply a pulsate effect
+   */
+  #shake() {
+    $('#before').on('click', () => {
+      $('#before').effect('shake', 'left', () => {
+        $('#before').hide();
+        $('#after').show();
+      });
+    });
+    $('#after').on('click', () => {
+      $('#after').effect('pulsate', 4);
+    });
+    $('#reset').on('mousedown', () => {
+      $('#after').hide();
+      $('#before').show();
+    });
+  }
+  
+  /** On mouseover an image it switch to another one  */
+  #over() {
+    let imageNumber = 3;
+    $('#imag').on('mouseover', () => {
+      $('#imag').toggle('drop');
+      let imageString = '../img/japan' + imageNumber + '.jpg';
+      imageNumber = imageNumber < 6 ? imageNumber : 2;
+        //$('#images').append('<img src="imageString" id="imag">');
+        imageNumber++;
+        $('#imag').attr('src', imageString).toggle('drop');
+    });
+  }
+}
+let shake = new Shake();
+
+
