1edfef55af95e25e787b64cebf4d34116534d1da
Adrian-alu0101024363 <alu0101024363@ull.edu.es>
2021-05-21 16:23:20 +0000

    changes

--- src/js/shake.js
+++ src/js/shake.js
@@ -50,5 +50,3 @@ export class Shake {
   }
 }
 let shake = new Shake();
-
-
--- web-server/app.js
+++ web-server/app.js
@@ -2,11 +2,11 @@ const EXPRESS = require('express');
 const PATH = require('path');
 const APP = EXPRESS();
 
-//set the port
+// set the port
 APP.set('port', 8080);
 
-//tell express that we want to use the www folder
-//for our static assets
+// tell express that we want to use the www folder
+// for our static assets
 APP.use('/', EXPRESS.static(PATH.join(__dirname, './')));
 APP.use(EXPRESS.static(PATH.join(__dirname, '../src')));
 APP.use('/jquery', EXPRESS.static(PATH.join(__dirname, '../node_modules/jquery/dist/')));
