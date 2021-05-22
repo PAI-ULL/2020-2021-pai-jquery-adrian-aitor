697144bff2b30bcfb1cc4b0058db882cdaae646c
Aitor Hernández Sánchez <alu0100987522@ull.edu.es>
2021-05-22 15:47:18 +0000

    Added jQuery UI plugin: accordion

---
+++ src/js/plugins.js
@@ -0,0 +1,14 @@
+/**
+  @author  Adrian Fleitas
+  @author Aitor Hernández Sánchez
+  @since  28.April.2021
+  @desc AJAX example to treat with an API type like
+  @link https://github.com/PAI-ULL/2020-2021-pai-jquery-adrian-aitor.git
+*/ 
+
+'use strict';
+
+/**
+ * Instantiation of an accordion plugin with the jQuery UI plugin
+ */
+$('#accordion').accordion();
